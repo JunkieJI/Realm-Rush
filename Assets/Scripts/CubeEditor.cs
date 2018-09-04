6e450e837d7f0749dfeed7a356213c9235ca0fff
Jihaad Pienaar <jihaad.pienaar@spandigital.com>
2018-09-04 14:28:47 +0200

    S5L139 - Added and applied production assets. Added skybox. Added towers that track enemies

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof (Waypoint))]
+public class CubeEditor : MonoBehaviour {
+    TextMesh textMesh;
+	Waypoint waypoint;
+	int gridSize;
+	void Awake() {
+		waypoint = GetComponent<Waypoint>();
+		gridSize = waypoint.GetGridSize();
+	}
+
+    void Update() {
+        SnapGrid();
+		UpdateLabel();
+    }
+
+	void SnapGrid() {
+        transform.position = new Vector3(
+			waypoint.GetGridPos().x * gridSize,
+			0f,
+			waypoint.GetGridPos().y * gridSize
+		);
+	}
+
+	void UpdateLabel() {
+		textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y;
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+	}
+}
