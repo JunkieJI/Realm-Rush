96591032612cded9aa0ad0db8c0d6f2007fc9b61
Jihaad Pienaar <jihaad.pienaar@spandigital.com>
2018-09-17 13:50:46 +0200

    S5L158 - Added ui health and score indicators

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
 
@@ -8,17 +9,23 @@ public class EnemySpawner : MonoBehaviour {
     [SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab;
     [SerializeField] Transform enemyParentTransform;
+    [SerializeField] Text spawnedEnemies;
+
+    int score;
 
 	// Use this for initialization
 	void Start ()
     {
         StartCoroutine(RepeatedlySpawnEnemies());
+        spawnedEnemies.text = score.ToString();
 	}
 	
     IEnumerator RepeatedlySpawnEnemies()
     {
         while (true) // forever
         {
+            score++;
+            spawnedEnemies.text = score.ToString();
             EnemyMovement newEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             newEnemy.transform.parent = enemyParentTransform;
             yield return new WaitForSeconds(secondsBetweenSpawns);
